bbbf8ad5354b9e02853a4327d393ef4873e0c09b
Tariq Hijazi <Tariq.t.m.hijazi@gmail.com>
2022-10-04 01:03:57 +0300

    h

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { LogInFormikyup } from "./src/screens/";
+import { Appradio } from "./src/screens/";
+import { Appregex } from "./src/screens/";
+import { SignUpFormik } from "./src/screens/";
+// import { Formikyup } from "./src/screens/Formikyup";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <SignUpFormik />
+
+}
